af643b55f2a4ba25d7ed96495bdea01ecb417eb0
Robin <robinlethiec1@gmail.com>
2022-12-20 23:46:50 +0100

    t

---
+++ cpp00/ex01/phonebook.hpp
@@ -0,0 +1,21 @@
+#ifndef __PHONEBOOK__
+	#define __PHONEBOOK__
+
+	class Contact
+	{
+		private :
+			char *first_name;
+			char *last_name;
+			char *nickname;
+			char *phone_number;
+			char *secret;
+	};
+
+	class Phonebook
+	{
+		public:
+			Phonebook();
+		
+	};
+
+#endif
