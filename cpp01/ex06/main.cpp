2489f95de58ba9f011f52854ca01d4ce6c3a2f78
Robin <robinlethiec1@gmail.com>
2023-01-08 19:06:21 +0100

    harlfilter

---
+++ cpp01/ex06/main.cpp
@@ -0,0 +1,56 @@
+#include "Harl.hpp"
+
+int	ft_error(std::string str)
+{
+	std::cout << str << std::endl;
+	return (0);
+}
+
+int main(int ac, char **av)
+{
+	if (ac != 2)
+		return (ft_error("Error\nWrong argument"));
+	
+	Harl	h;
+	std::string	str = av[1];
+	int	c;
+	std::string	tab[4] =
+	{
+		"DEBUG",
+		"INFO",
+		"WARNING",
+		"ERROR"
+	};
+
+	for (int i = 0; i < 4; i++)
+	{
+		if (tab[i] == str)
+			c = i;
+	}
+	switch (c)
+	{
+	case 0:
+		h.complain("DEBUG");
+		h.complain("INFO");
+		h.complain("WARNING");
+		h.complain("ERROR");
+		break;
+	case 1:
+		h.complain("INFO");
+		h.complain("WARNING");
+		h.complain("ERROR");
+		break;
+	case 2:
+		h.complain("WARNING");
+		h.complain("ERROR");
+		break;
+	case 3:
+		h.complain("ERROR");
+		break;	
+	default:
+		std::cout << "Error\nWrong option" << std::endl;
+		break;
+	}
+
+	return (0);
+}
