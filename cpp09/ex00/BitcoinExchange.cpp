0d10c9e22ab5b567093a1f97773c41ddcae5c3df
Robin <robinlethiec1@gmail.com>
2023-03-15 15:24:15 +0100

    ex00

--- cpp09/ex00/BitcoinExchange.cpp
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -35,8 +35,8 @@ BitcoinExchange::BitcoinExchange(char *inputFile)
 		{
 			std::pair<std::string, std::string> datee;
 			datee = _parse_line(buff);
-			// std::cout << buff << std::endl;
 			_cmp_data(datee);
+			// _cmp_data(_parse_line(buff));
 		}
 		catch(const std::exception& e)
 		{
@@ -52,20 +52,27 @@ BitcoinExchange::~BitcoinExchange()
 
 void	BitcoinExchange::_cmp_data(std::pair<std::string, std::string> date)
 {
-	for (std::map<std::string, std::string>::iterator it = _bitcoin_price.begin(); it != _bitcoin_price.end(); ++it)
-	{
-		if (date.first == it->first)
-		{
-			std::cout << date.first << " => " << date.second << " = ";
-			char *convertme1 = &date.second[0];
-			char *convertme2 = &it->second[0];
-			double converted1=atof(convertme1);
-			double converted2=atof(convertme2);
-			std::cout << converted1*converted2 << std::endl;
-			return;
-		}
-	}
-	throw std::length_error("Error: can't find date in database");
+	// for (std::map<std::string, std::string>::iterator it = _bitcoin_price.begin(); it != _bitcoin_price.end(); ++it)
+	// {
+	// 	if (date.first == it->first)
+	// 	{
+	// 		std::cout << date.first << " => " << date.second << " = ";
+	// 		char *convertme1 = &date.second[0];
+	// 		char *convertme2 = &it->second[0];
+	// 		double converted1=atof(convertme1);
+	// 		double converted2=atof(convertme2);
+	// 		std::cout << converted1*converted2 << std::endl;
+	// 		return;
+	// 	}
+	// }
+	// throw std::length_error("Error: can't find date in database");
+	std::map<std::string, std::string>::iterator it = _bitcoin_price.lower_bound(date.first);
+	// std::cout << (*_bitcoin_price.lower_bound(date.first)).second << std::endl;
+	char *convertme1 = &date.second[0];
+	char *convertme2 = &it->second[0];
+	double converted1=atof(convertme1);
+	double converted2=atof(convertme2);
+	std::cout << date.first << " => " << date.second << " = " << converted1*converted2 << std::endl;
 }
 
 void	BitcoinExchange::_fillcsv(std::string line)
@@ -90,6 +97,72 @@ void	BitcoinExchange::_fillcsv(std::string line)
 		line2.erase(0, pos + delimiter.length());
 	}
 	_bitcoin_price.insert(std::pair<std::string, std::string>(token, line2));
+}
+
+void	pyear(std::string year)
+{
+	if (year.size() != 4)
+		throw std::length_error("year error !");
+	char *str = &year[0];
+	int		y = atoi(str);
+	if (y < 2009)
+		throw std::length_error("year error !");
+	else if (y > 2022)
+		throw std::length_error("year error !");
+}
+
+void	pmonth(std::string month)
+{
+	if (month.size() != 2)
+		throw std::length_error("month error !");
+	char *str = &month[0];
+	int		y = atoi(str);
+	if (y < 1)
+		throw std::length_error("month error !");
+	else if (y > 12)
+		throw std::length_error("month error !");
+}
+
+void	pday(std::string day)
+{
+	if (day.size() != 2)
+		throw std::length_error("day error !");
+	char *str = &day[0];
+	int		y = atoi(str);
+	if (y < 1)
+		throw std::length_error("day error !");
+	else if (y > 31)
+		throw std::length_error("day error !");
+}
+
+void	check_date(std::string full_date)
+{
+	int c = 0;
+	int i = 0;
+	while (full_date[i])
+	{
+		if (full_date[i] == '-')
+			c++;
+		i++;
+	}
+	if (c != 2)
+		throw std::length_error("Error: bad input => " + full_date);
+	i = 0;
+	std::string	date = full_date;
+	std::string delimiter = "-";
+	size_t pos = 0;
+	std::string token;
+	while ((pos = date.find(delimiter)) != std::string::npos) {
+		token = date.substr(0, pos);
+		if (i == 0)
+			pyear(token);
+		else if (i == 1)
+			pmonth(token);
+		date.erase(0, pos + delimiter.length());
+		i++;
+	}
+	pday(date);
+	
 }
 
 std::pair<std::string, std::string>	BitcoinExchange::_parse_line(std::string line)
@@ -119,6 +192,7 @@ std::pair<std::string, std::string>	BitcoinExchange::_parse_line(std::string lin
 		token = line2.substr(0, pos);
 		line2.erase(0, pos + delimiter.length());
 	}
+	check_date(token);
 	std::string	snbr = line2;
 	
 	if (snbr.size() > 7)
@@ -138,6 +212,12 @@ std::pair<std::string, std::string>	BitcoinExchange::_parse_line(std::string lin
 			throw std::length_error("Error: not a valid number.");
 		i++;
 	}
+	char *nb = &snbr[0];
+	double c1=atof(nb);
+	if (c1 < 0)
+		throw std::length_error("Error: negative number");
+	if (c1 > 1000)
+		throw std::length_error("Error: too hight");
 	
 	// _parse_nbr(token);
 
@@ -175,42 +255,6 @@ std::pair<std::string, std::string>	BitcoinExchange::_parse_line(std::string lin
 // 	}
 // 	// _parse_nbr(token);
 
-// }
-
-// void	pyear(std::string year)
-// {
-// 	if (year.size() != 4)
-// 		throw std::length_error("year error !");
-// 	char *str = &year[0];
-// 	int		y = atoi(str);
-// 	if (y < 2009)
-// 		throw std::length_error("year error !");
-// 	else if (y > 2022)
-// 		throw std::length_error("year error !");
-// }
-
-// void	pmonth(std::string month)
-// {
-// 	if (month.size() != 2)
-// 		throw std::length_error("month error !");
-// 	char *str = &month[0];
-// 	int		y = atoi(str);
-// 	if (y < 1)
-// 		throw std::length_error("month error !");
-// 	else if (y > 12)
-// 		throw std::length_error("month error !");
-// }
-
-// void	pday(std::string day)
-// {
-// 	if (day.size() != 2)
-// 		throw std::length_error("day error !");
-// 	char *str = &day[0];
-// 	int		y = atoi(str);
-// 	if (y < 1)
-// 		throw std::length_error("day error !");
-// 	else if (y > 31)
-// 		throw std::length_error("day error !");
 // }
 
 // void BitcoinExchange::_parse_date(std::string date)
